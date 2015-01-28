422ab75a4f095fb33e286dda5894b7e64c6da6b2
Kyle Fowler <ktfowler1337@gmail.com>
2015-01-28 17:23:23 -0500

    music and icon

--- LinkTheBoomerangMaster/LinkTheBoomerangMaster/Classes/GameObject.cs
+++ LinkTheBoomerangMaster/LinkTheBoomerangMaster/Classes/GameObject.cs
@@ -17,6 +17,8 @@ namespace LinkTheBoomerangMaster
         public bool isMoving = false;
         public AnimatedTexture animatedTexture;
 
+        private float scaleOveride = 0;
+
         public GameObject(float scale = 0)
         {
             if (scale == 0)
